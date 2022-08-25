c5b52ed3e7220fa8b1cc61465e0ba28453cb93e2
Davi Moreira da Costa <davimoreira7891@gmail.com>
2022-08-25 10:23:25 -0300

    mensagem

---
+++ site/src/pages/home/index.js
@@ -0,0 +1,44 @@
+import './index.scss'
+
+
+export default function Index() {
+
+    return(
+        <main className='home'>
+            <section className='home-f1'>
+                <header className='home-header'>
+                    <h4>Fazer login</h4> 
+
+                    <img src=''></img>
+
+                    <h4>Registre-se</h4>
+
+                </header>
+
+                <div className='cont-1-home'>
+                    <div>
+                        <h1 className='cont-1-h1'>Faça parte do nosso precioso sonho</h1>
+
+                        <p className=''>Apresentamos para você um serviço rápido e prático facilitando o seu convívio com consultas e trabalhos.</p>
+
+
+                        <div>
+                            <input placeholder='BUSCAR EMPRESAS'/>
+                            <span className='logo'></span> 
+                        </div>
+                    </div>
+
+                    <div>
+                        <img></img>
+
+                    </div>
+
+                </div>
+            </section>
+
+
+
+
+        </main>
+    )
+}
