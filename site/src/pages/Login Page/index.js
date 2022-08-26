ec20aaa2ead543b3d85469c9c9682239d888c3ca
Thomaz Erick Assis dos Santos <thomaz_erick@hotmail.com>
2022-08-26 16:13:30 -0300

    Implementando Page Login !

---
+++ site/src/pages/Login Page/index.js
@@ -0,0 +1,42 @@
+import './index.scss';
+
+export default function(){
+  
+return(
+
+<main>
+    <div className="Div-Group">
+        <div className="Div-1"> 
+            <div className='Div-1-div'>
+                  <h1>My Workship </h1>
+            </div>
+            
+            <div className='Espac'></div>
+            
+            <div className='Div1-Img'>
+                    <div className= 'Imagem-calendario' />
+            </div>
+                
+                <div className='Div-2'>
+                    <div> 
+                        <h1>Bem vindo</h1>
+                    
+                        
+                            <p> A Networking que você precisa! Intereja diretamente 
+                                    com centenas de serviços de empresas  diferentes 
+                                        quando você quiser.</p>
+                        
+                </div>
+                    
+                    </div>
+                    
+        
+        </div>
+        
+        
+        <div className='Div-5'> </div>
+    </div>
+</main>  
+       
+)
+}
