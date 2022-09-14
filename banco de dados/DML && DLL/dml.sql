8999f8f300c95e8d00bfde06eddc82ca42734931
adaylsonevaristo15@gmail.com <adaylsonevaristo15@gmail.com>
2022-09-14 09:33:03 -0300

    alterações dml

--- banco de dados/DML && DLL/dml.sql
+++ banco de dados/DML && DLL/dml.sql
@@ -1,12 +1,14 @@
 use MyWorkshipDB;
 
-/*--------------------INFORMAÇÕES_USUARIO--------------------------*/
+/*--------------------INFORMAÇÕES_USUARIO_EMPRESA--------------------------*/
 
 insert into TB_USUARIO_EMPRESA (DS_CNPJ,DS_INSCRICAO_ESTADUAL,NM_NOME_DA_EMPRESA,DS_ESTADO,NM_REPRESENTANTE,DS_CPF_REPRESENTANTE,DS_CARGO_REPRESENTANTE,DS_NACIONALIDADE_REPRESENTANTE)
 values("18638446000101", "739733422933", "MyWorkship", "São Paulo", "Akino Rego", "57348472087", "Gerente", "Brasil");
 
 select *
 	from TB_USUARIO_EMPRESA;
+
+/*--------------------LOGIN_EMPRESA--------------------------*/
     
 
 insert into TB_LOGIN_EMPRESA(DS_EMAIL,DS_SENHA,DT_ULTLOGIN)
