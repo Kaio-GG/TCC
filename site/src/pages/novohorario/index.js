23438827e4b0db3900605b11589ca068b41b2166
kaio felipe <kaiofdss@gmail.com>
2022-09-16 11:55:25 -0300

    oi

---
+++ site/src/pages/novohorario/index.js
@@ -0,0 +1,16 @@
+import './index.scss'
+import Cardnovohorario from '../../components/card-novohoraio/index.js'
+
+
+
+export default function Index (){
+
+
+    return(
+        <div className='pg-novohorario'>
+            <Cardnovohorario/>
+           
+
+        </div>
+    )
+}
