4f36c3bf7b7ced12677b074ac8bdb92da32dbd94
Thomaz Erick Assis dos Santos <thomaz_erick@hotmail.com>
2022-09-18 14:10:15 -0300

    Page Escolher Cadastro 5%

--- site/src/pages/EscolherCadastro/index.js
+++ site/src/pages/EscolherCadastro/index.js
@@ -4,10 +4,14 @@ import './index.scss'
 export default function Index(){
 
     return(
-        <main>
-            <section>
-                <p> feito!</p>
-
+        
+        <main className='Main-Escolher-Cadastro'>
+            
+            <section className='Faixa-1'>
+                <div className='Div-Group'>
+                    <h1> My Workship</h1> 
+                    <p> Linguagem</p>   
+                 </div>
             </section>
 
         </main>
