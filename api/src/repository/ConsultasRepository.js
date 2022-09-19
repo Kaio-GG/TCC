926441c832f381bd8716a1b8d148f3ac8029f8d4
Davi Moreira da Costa <davimoreira7891@gmail.com>
2022-09-19 09:33:37 -0300

    oii 2

--- api/src/repository/ConsultasRepository.js
+++ api/src/repository/ConsultasRepository.js
@@ -5,12 +5,19 @@ import { con } from './connection.js'
 
 export async function empresasBemAvaliadas() {
     const comando = `
-    
-    
-    
+
+    SELECT nm_usuario nome, 
+    vl_avaliacao avaliacao, 
+    ds_avaliacao descavaliacao
+
+
+    FROM tb_empresa_avaliacao
+    INNER JOIN 
+    TB_USUARIO_CLIENTE 
+    on tb_empresa_avaliacao.id_usuario_cliente = TB_USUARIO_CLIENTE.id_usuario_cliente
     `
 
-
-
+    const [lista] = await con.query(comando);
+    return lista;
 
 }
