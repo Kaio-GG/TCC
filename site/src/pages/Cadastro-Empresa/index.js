800a3890e9fad5f454a31b05863ebc8ac9a5e9cd
Thomaz Erick Assis dos Santos <thomaz_erick@hotmail.com>
2022-09-20 09:43:58 -0300

    Implementação Page Cadastro empresa iniciada !

---
+++ site/src/pages/Cadastro-Empresa/index.js
@@ -0,0 +1,25 @@
+import './index.scss'
+
+export default function(){
+return(
+    <main className='Main-Cadastro-Empresa'>
+        
+        <section className='Faixa-1'>
+            <div className='Div-Group'>
+                <h1> My Workship</h1> 
+                <h2> Linguagem</h2>   
+            </div>
+         </section>
+
+         <section className='Div-Bolinhas'> 
+                <div className='Div-Container-Bolinhas'> 
+                    <div> </div> ; <hr></hr> ; <div> </div> ; <hr></hr> , 
+                </div>
+
+         </section>
+
+
+
+    </main>
+)
+}
