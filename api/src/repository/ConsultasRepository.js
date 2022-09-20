48f2776a49dbfb80d0d49f8c4fe3c7df8196234e
Davi Moreira <davimoreira7891@gmail.com>
2022-09-19 21:25:55 -0300

    Deixei a função limitada a 4 e randomizada

--- api/src/repository/ConsultasRepository.js
+++ api/src/repository/ConsultasRepository.js
@@ -18,6 +18,8 @@ export async function empresasBemAvaliadas() {
 
     INNER JOIN TB_USUARIO_EMPRESA on tb_empresa_avaliacao.id_usuario_empresa =
     TB_USUARIO_EMPRESA.id_usuario_empresa
+    ORDER BY rand()
+    LIMIT 4
     `
 
     const [lista] = await con.query(comando);
