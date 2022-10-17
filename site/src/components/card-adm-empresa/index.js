ee81633fc96b5d943e15c7f16da804435010908e
Kaio felipe <kaiofdss@gmail.com>
2022-10-17 09:44:44 -0300

    ajustes

--- site/src/components/card-adm-empresa/index.js
+++ site/src/components/card-adm-empresa/index.js
@@ -21,15 +21,15 @@ export default function Cardadm (props){
     return(
         <div className="card">
             <div className='nome'> {props.item.nome}  </div>
-            <div> {props.item.hora} </div>
-            <div> {String(props.item.data).substr(0,10)}  </div>
-            <div> {props.item.local} </div>
+            <div className='nome' > {props.item.hora} </div>
+            <div className='nome'> {String(props.item.data).substr(0,10)}  </div>
+            <div className='nome'> {props.item.local} </div>
             <div className='imagens'>
                 <img src='/assets/images/verificar.svg' alt='' onClick={() => aceitarAgendamento(props.item.id)} />
                 <img src='/assets/images/excluir.svg' alt='' onClick={() => agendamentoRecusado(props.item.id)} />
             </div>
-            <div> {props.item.situacao} </div>    
-            <div onClick={() => info(props.item.id)}> INFORMAÇÕES</div>
+            <div className='nome1'> {props.item.situacao} </div>    
+            <div className='nome' onClick={() => info(props.item.id)}> INFORMAÇÕES</div>
         </div>
     )
 }
