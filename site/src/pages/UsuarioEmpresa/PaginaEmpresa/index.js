fcacef09aa7ddbe7798d08b88d908b975ce0493f
Adaylson Arcanjo <adaylsonevaristo15@gmail.com>
2022-10-17 07:37:29 -0300

    Links PaginaEmpresa

---
+++ site/src/pages/UsuarioEmpresa/PaginaEmpresa/index.js
@@ -0,0 +1,62 @@
+import './index.scss';
+import { Link } from 'react-router-dom';
+import HeaderEmpresa from '../../../components/header-adm-empresa';
+
+export default function PaginaEmpresa() {
+
+    return(
+        <main className="PaginaEmpresa">
+            <div className="UsuarioEmpresaPagina-Centro">
+                <HeaderEmpresa />
+                <div className='agrups'>
+                    <div className="agrup-esquerda">
+
+                        <div className="card-empresa">
+                            <div className='a'>
+                                <div className="img">
+                                    <div className='aimg'></div>
+                                </div>
+                                <div className="nome-desc">
+                                    <h3 className="nome">NUTRIFIT</h3>
+                                    <p className="desc"> é uma empresa de nutricionistas qualificados, que buscam o bem estar de seus clintes. Sempre com qualidade no atendimento e um sorriso no rosto  </p>
+                                </div>
+                            </div>
+                            <div className="ava-locais">
+                                <div className='estrelas'>
+                                    <div className='estrela'></div>
+                                    <div className='estrela'></div>
+                                    <div className='estrela'></div>
+                                    <div className='estrela'></div>
+                                    <div className='estrela'></div>
+                                </div>
+                                <p> Brasil, São Paulo, Sp </p>
+                                <p> Rua seila, 32 </p>
+                            </div>
+                        </div>
+
+                        <div className="card-Agendar">
+                            <p>Adicionar Card</p>
+                            <button className='agendar'><Link to={'./agendar'}>Agendar</Link></button>
+                        </div>
+
+                    </div>
+                    <div className="agrup-direita">
+
+                        <div className="verificacao">
+                            <h3> verificaçâo</h3>
+                            <p> facebook </p>
+                            <p> email </p>
+                            <p> Youtube </p>
+                        </div>
+
+                        <div className="certificacao">
+                            <h3> certificação</h3>
+                            <p> CNPJ </p>
+                            <p> Certificados </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
