e032107ef0373422810af741ea769eab47348695
Kaio Felipe <kaiofdss@gmail.com>
2022-10-18 18:31:18 -0300

    ajustes 3.0

--- site/src/pages/UsuarioEmpresa/adm-empresa-home/index.js
+++ site/src/pages/UsuarioEmpresa/adm-empresa-home/index.js
@@ -1,17 +1,16 @@
 import './index.scss'
 import Cardadm from '../../../components/card-adm-empresa'
-import Filtroemp from '../../../components/filtro-adm-empresa'
 import HeaderEmp from '../../../components/header-adm-empresa/index.js'
 import { useEffect, useState  } from 'react'
 import Storage from 'local-storage'
-import { agendamentosData , agendamentos } from '../../../api/agendamentos.js'
+import { buscarLocal , agendamentosData , agendamentos } from '../../../api/agendamentos.js'
 
 
 export default function Homeempresa (){
     const [id , setid] = useState(3)
     const [agendamento , setagendamento ] = useState([])
     const [data , setdata] = useState('')
-    
+    const [local, setlocal] =useState([])    
  
      function novaData (){
          const a = new Date()
@@ -21,17 +20,32 @@ export default function Homeempresa (){
          setdata(`${d}-${c}-${b}`)
      }
  
- 
+     
+     async function ListarPorData (dt , id){
+        const r = await agendamentosData(dt , id)
+        setagendamento(r)
+     }
+
  
     async function listar (id){
         const r = await agendamentos(id)
         setagendamento(r)
         console.log(agendamento)
     }
+    async function buscar (id){
+        try {
+          const a = await buscarLocal(id)  
+          setlocal(a)
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+
     useEffect(() => {
         const empresaLogada = Storage('Empresa-Logada')
         setid(empresaLogada.ID_USUARIO_EMPRESA)
         listar(id)
+        buscar(id)
         novaData()
     },[])       
 
@@ -41,10 +55,28 @@ export default function Homeempresa (){
     return (
         <div className='page-adm-home'>
             <HeaderEmp class='home' />
-            <Filtroemp className='filtro'   data={data}/>    
+            <div className='filtro-adm-empresa'>
+            <div className='btn'>
+            <select className='opt' value={local} onChange={e => setlocal(e.target.value)} >               
+                    
+                        
+                    {local.map (item =>
+                        <option value={item.local}>{item.local}</option>
+                        )}
+                        
+                    
+                    
+                    </select>
+            </div>
+            <div className='btn' style={{marginLeft:'5%'}}>
+                <div></div>
+                    <input placeholder='DATA' type='date' value={data} onChange={e => setdata(e.target.value)} />
+                    <button onClick={ ()=> ListarPorData(id , data) } >Buscar</button>
+            </div>
+        </div>  
 
             {agendamento.map(item => 
-                <Cardadm item={item} />    
+                <Cardadm item={item} onClick={() => listar(id)} />    
             )}
         </div>
     )
